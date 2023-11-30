87394c4d1eaf088d92c0e1ab2e511bd8af369873
Dom Sip <dominik.sipowicz@gmail.com>
2023-11-30 14:09:59 +0000

    add more testing for rsc

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,15 @@
 'use client'
 
+import Link from 'next/link'
 const IndexPage = () => (
   <div>
     <h1>Minimal Next</h1>
+    <h2>Links</h2>
+    <ul>
+      <li><a href="/1">/1 (normal link)</a></li>
+      <li><Link href="/2">/2 (next/link)</Link></li>
+      <li><Link href="/2" prefetch={false}>/2 (next/link  prefetch=false})</Link></li>
+    </ul>
   </div>
 )
 
